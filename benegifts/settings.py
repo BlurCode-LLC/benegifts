40f740f95bf80f986d941bf09215ac10ae0ce365
abror-mamataliev <abrornomos2018@gmail.com>
2023-03-06 18:39:05 +0000

    Deployed

--- benegifts/settings.py
+++ benegifts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j1rloklfu&h$ur#11sz&#4sm0am(vabo_6f1(8cy^44ft@+!o='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "benegifts.uz"
